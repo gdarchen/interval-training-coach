9d018f26f5b0a3ffa38033b158c2afb07cb0bbe8
Gautier Darchen <darchen.gautier@gmail.com>
2020-03-07 16:23:02 +0100

    Updates

---
+++ precache-manifest.2a49e2c08321d7bd35f76ef7dc750647.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a1346f3bd6e723d94bbcf1d15ee0b4",
+    "url": "/interval-training-coach/index.html"
+  },
+  {
+    "revision": "d3e94369354f27fdafa2",
+    "url": "/interval-training-coach/static/css/main.4b740d44.chunk.css"
+  },
+  {
+    "revision": "64a94ab61240ce2c66dc",
+    "url": "/interval-training-coach/static/js/0.40483148.chunk.js"
+  },
+  {
+    "revision": "6655801522e319a9572d",
+    "url": "/interval-training-coach/static/js/3.26c7acd8.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/interval-training-coach/static/js/3.26c7acd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92586bad81371a1d84bb",
+    "url": "/interval-training-coach/static/js/4.61804149.chunk.js"
+  },
+  {
+    "revision": "93457f1a4b97b3855ad4",
+    "url": "/interval-training-coach/static/js/5.9fdcc9ad.chunk.js"
+  },
+  {
+    "revision": "6751105bb545f6a487ba",
+    "url": "/interval-training-coach/static/js/6.6d3ff7ea.chunk.js"
+  },
+  {
+    "revision": "d3e94369354f27fdafa2",
+    "url": "/interval-training-coach/static/js/main.89f2c58b.chunk.js"
+  },
+  {
+    "revision": "958cc29b813cf47d1692",
+    "url": "/interval-training-coach/static/js/runtime-main.8398c0cf.js"
+  },
+  {
+    "revision": "7fc97f050f24bc048d5eae6a8447e916",
+    "url": "/interval-training-coach/static/media/profile.7fc97f05.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/interval-training-coach/precache-manifest.7d0fa2e96d61515c384e67b29d997e5e.js"
+  "/interval-training-coach/precache-manifest.2a49e2c08321d7bd35f76ef7dc750647.js"
 );
 
 self.addEventListener('message', (event) => {
