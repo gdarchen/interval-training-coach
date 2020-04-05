c13c0ccbcea8316c2faa21267b5e497e82e77a72
Gautier Darchen <darchen.gautier@gmail.com>
2020-04-05 15:47:37 +0200

    feat(reorder-periods): add handles to reorder periods in edit mode

--- src/common/intervals-list/IntervalList.jsx
+++ src/common/intervals-list/IntervalList.jsx
@@ -1,44 +1,66 @@
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import { makeStyles } from "@material-ui/core/styles";
+import arrayMove from "array-move";
+import ReorderIcon from "@material-ui/icons/Reorder";
 import classNames from "classnames";
 import React from "react";
 import { connect } from "react-redux";
-import { saveIntervalToEditAction } from "../../redux/actions/trainingActions";
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+} from "react-sortable-hoc";
+import {
+  saveIntervalToEditAction,
+  saveTrainingInCreationAction,
+} from "../../redux/actions/trainingActions";
 import { formatDuration } from "../../utils/durationUtils";
 import {
   formatIntervalToSpeechText,
-  speak
+  speak,
 } from "../../utils/textToSpeechUtils";
 import IntervalListItem from "./IntervalListItem";
 import IntervalOccurences from "./IntervalOccurence";
 
 const useStyles = makeStyles({
+  intervalListContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  period: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    color: "white",
+  },
   list: {
-    marginTop: "20px"
+    paddingTop: "20px",
   },
   paddedListItem: {
     "& > *:not(:last-child)": {
-      marginBottom: "2px"
-    }
+      marginBottom: "2px",
+    },
   },
   listItem: {
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   interval: {
-    cursor: "pointer"
+    cursor: "pointer",
   },
   occurencesListItem: {
     paddingTop: 0,
-    paddingBottom: 0
-  }
+    paddingBottom: 0,
+  },
 });
 
 const IntervalList = ({
   training,
   isEditMode,
-  saveIntervalToEditAction: saveIntervalToEdit
+  saveIntervalToEdit,
+  saveTrainingInCreation,
 }) => {
   const classes = useStyles();
 
@@ -46,67 +68,104 @@ const IntervalList = ({
     return null;
   }
 
-  return (
-    <div>
-      {training.periods.map((period, index) => {
-        return (
-          <List
-            key={`${index}-${JSON.stringify(period.group)}-${JSON.stringify(
-              period.occurences
-            )}`}
-            className={classes.list}
+  const DragHandle = SortableHandle(() => <ReorderIcon />);
+
+  const Period = ({ period, index }) => (
+    <div
+      key={`${index}-${JSON.stringify(period.group)}-${JSON.stringify(
+        period.occurences
+      )}`}
+      className={classes.period}
+    >
+      {isEditMode && <DragHandle />}
+      <List className={classes.list}>
+        <ListItem
+          className={classNames(classes.listItem, classes.paddedListItem)}
+        >
+          {period.group &&
+            period.group.map((interval, index) => {
+              return (
+                <div
+                  className={classes.interval}
+                  onClick={() =>
+                    isEditMode
+                      ? saveIntervalToEdit(interval)
+                      : speak(formatIntervalToSpeechText(interval))
+                  }
+                  key={`${index}-${JSON.stringify(interval)}-${JSON.stringify(
+                    interval.occurences
+                  )}`}
+                >
+                  <IntervalListItem
+                    intervalId={interval.id}
+                    formattedDuration={
+                      interval.duration
+                        ? formatDuration(interval.duration)
+                        : null
+                    }
+                    description={interval.description}
+                    isEditMode={isEditMode}
+                  />
+                </div>
+              );
+            })}
+        </ListItem>
+        {period.occurences > 1 && (
+          <ListItem
+            className={classNames(classes.listItem, classes.occurencesListItem)}
           >
-            <ListItem
-              className={classNames(classes.listItem, classes.paddedListItem)}
-            >
-              {period.group &&
-                period.group.map((interval, index) => {
-                  return (
-                    <div
-                      className={classes.interval}
-                      onClick={() =>
-                        isEditMode
-                          ? saveIntervalToEdit(interval)
-                          : speak(formatIntervalToSpeechText(interval))
-                      }
-                      key={`${index}-${JSON.stringify(
-                        interval
-                      )}-${JSON.stringify(interval.occurences)}`}
-                    >
-                      <IntervalListItem
-                        intervalId={interval.id}
-                        formattedDuration={
-                          interval.duration
-                            ? formatDuration(interval.duration)
-                            : null
-                        }
-                        description={interval.description}
-                        isEditMode={isEditMode}
-                      />
-                    </div>
-                  );
-                })}
-            </ListItem>
-            {period.occurences > 1 && (
-              <ListItem
-                className={classNames(
-                  classes.listItem,
-                  classes.occurencesListItem
-                )}
-              >
-                <IntervalOccurences occurences={period.occurences} />
-              </ListItem>
-            )}
-          </List>
-        );
-      })}
+            <IntervalOccurences occurences={period.occurences} />
+          </ListItem>
+        )}
+      </List>
+    </div>
+  );
+
+  const SortablePeriod = SortableElement(Period);
+
+  const IntervalList = () => (
+    <div>
+      {training &&
+        training.periods &&
+        training.periods.map((period, index) => {
+          const PeriodToRender = isEditMode ? SortablePeriod : Period;
+          return (
+            <PeriodToRender
+              key={`sortable-item-${index}-${JSON.stringify(
+                period.group
+              )}-${JSON.stringify(period.occurences)}`}
+              period={period}
+              index={index}
+            />
+          );
+        })}
+    </div>
+  );
+
+  const SortableIntervalList = SortableContainer(IntervalList);
+
+  const reorderPeriods = ({ oldIndex, newIndex }) => {
+    const reorderedTraining = arrayMove(training.periods, oldIndex, newIndex);
+    saveTrainingInCreation({ ...training, periods: reorderedTraining });
+    console.log(oldIndex, newIndex);
+  };
+
+  return (
+    <div className={classes.intervalListContainer}>
+      {isEditMode ? (
+        <SortableIntervalList onSortEnd={reorderPeriods} useDragHandle />
+      ) : (
+        <IntervalList />
+      )}
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  saveIntervalToEditAction: interval =>
-    dispatch(saveIntervalToEditAction(interval))
+const mapDispatchToProps = (dispatch) => ({
+  saveIntervalToEdit: (interval) =>
+    dispatch(saveIntervalToEditAction(interval)),
+  saveTrainingInCreation: (training) =>
+    dispatch(saveTrainingInCreationAction(training)),
 });
 
 export default connect(null, mapDispatchToProps)(IntervalList);
